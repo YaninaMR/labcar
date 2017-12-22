ef2f272e630f161a69582512f8abcb78fdcfb5c7
YaninaMR <mercadoyanic@gmail.com>
2017-12-22 12:59:41 -0500

    adding app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,60 @@
 // La funcionalidad de tu proyecto
+	$('#buyer').click(function(){
+	   var $name = $('#div-save');
+	   $name.append("soy un span");
+	   alert("se ha creado un nuevo p"); 
+	});
+	$('#registrar-d').click(function(){
+		var $name = $('#div-save');
+		$name.append("soy un span"); 
+	 });
+
+	$('#registrar-d').click(function(){
+		var $name = $('#first-data').val();
+		var $namediv = $('#div-save');
+		$namediv.append($name); 
+	 });
+
+  /*$("#myModalWindowRegister").on("submit", "#enviar-datos" function(){
+    
+      var $name = $('#nombre').val();
+		var $namediv = $('#div-save');
+		$namediv.append($name);
+        
+    });*/
+
+  
+ 
+    $('#enviar-datos').click(function(e){
+    	e.preventDefault();
+        var $name = $('#nombre').val();	
+        var tabla_div = $('#div-save'); 
+        $( "body" ).find( tabla_div ).append('<p>' + $name +'</p>');           
+        $( "body" ).find( tabla_div ).wrap('<p>' + $name +'</p>');
+     });
+
+
+
+
+
+	
+   /*
+    $(function() {
+        $('#html').html('<strong>Ejemplo con html()</strong>');
+        $('#text').text('Ejemplo con text()');
+        $('#val').val('Ejemplo con val()');
+        $('#append').append('append()');
+      });
+   */
+    
+
+
+
+
+
+
+
+
+
+
+
